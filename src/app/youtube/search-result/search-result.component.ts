72eaa7f5439ad3943cc7600ec7b2035c82d474be
Aghaee <saeed.aghaee@accenture.com>
2019-02-04 22:06:54 +0000

    Replace item with card

--- src/app/youtube/search-result/search-result.component.ts
+++ src/app/youtube/search-result/search-result.component.ts
@@ -1,30 +1,35 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { VideoDetail } from '../video-detail.model';
-import { YoutubeVideoPlayer } from '@ionic-native/youtube-video-player/ngx';
-import { Subject } from 'rxjs';
+import { Component, OnInit, Input } from "@angular/core";
+import { VideoDetail } from "../video-detail.model";
+import { YoutubeVideoPlayer } from "@ionic-native/youtube-video-player/ngx";
 
-import { YoutubeDownloadService } from '../../services/youtube-download.service';
+import { YoutubeDownloadService } from "../../services/youtube-download.service";
 
 @Component({
-  selector: 'app-search-result',
-  templateUrl: './search-result.component.html',
-  styleUrls: ['./search-result.component.scss']
+  selector: "app-search-result",
+  templateUrl: "./search-result.component.html",
+  styleUrls: ["./search-result.component.scss"]
 })
 export class SearchResultComponent implements OnInit {
   @Input() result: VideoDetail;
 
-  constructor(private youtubeDownloadService: YoutubeDownloadService) { }
+  constructor(
+    private youtubeVideoPlayer: YoutubeVideoPlayer,
+    private youtubeDownloadService: YoutubeDownloadService
+  ) {}
 
   ngOnInit() {
     this.youtubeDownloadService.downloads.subscribe(msg => {
-      if (msg.type == 'download-finished') {
-
+      if (msg.type === "download-finished") {
       }
       console.log(msg);
-    })
+    });
   }
 
   downloadVideo(videoId) {
     this.youtubeDownloadService.downloadVideo(videoId);
+  }
+
+  playVideo(videoId) {
+    this.youtubeVideoPlayer.openVideo(videoId);
   }
 }
